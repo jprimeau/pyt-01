0d9c68e6638f2b56b17dda0657f66b0731998c99
Jonathan Primeau <jprimeau@spiria.com>
2016-01-31 21:32:06 -0500

    Added a new kata (my_turn).

---
+++ kata/07_my_turn/my_turn.py
@@ -0,0 +1,3 @@
+# Create your own kata! Find a good idea then write some explanation below.
+# Don't forget to write some unit test to test your idea.
+# Your colleague will train by using your kata!
---
+++ kata/07_my_turn/test_my_turn.py
@@ -0,0 +1,1 @@
+# Insert your unit tests here.
