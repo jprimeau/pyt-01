6a034427d74c2ec9bc30352ef4235396f91774b0
Jonathan Primeau <jprimeau@spiria.com>
2016-02-01 14:21:46 -0500

    added more output tests for cheating kata

--- kata/06_cheating/test_cheating.py
+++ kata/06_cheating/test_cheating.py
@@ -7,6 +7,7 @@ def test_output():
     assert removeNb(100) == []
     assert removeNb(26) == [(15, 21), (21, 15)]
     assert removeNb(32) == []
+    assert removeNb(906) == [(505, 811), (615, 666), (637, 643), (643, 637), (666, 615), (811, 505)]
     assert removeNb(990) == [(517, 946), (946, 517)]
     assert removeNb(2720) == [(1548, 2388), (2388, 1548)]
     assert removeNb(10000) == []
